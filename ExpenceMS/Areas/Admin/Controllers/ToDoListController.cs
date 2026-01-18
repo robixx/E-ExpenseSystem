45ea85d0f8239952ab01b640ffb70706dfdf9d1a
Md. Shoriful Islam <shorifulislam776@gmail.com>
2026-01-18 23:01:17 +0600

    todolist pagecreate

---
+++ ExpenceMS/Areas/Admin/Controllers/ToDoListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenceMS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class ToDoListController : Controller
+    {
+        [HttpGet]
+        public IActionResult ToDoIndex()
+        {
+            return View();
+        }
+    }
+}
